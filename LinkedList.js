1ca1323c113f99a78828f3d9565adf2ca0d52c3b
bello hadi <bellohadi82@gmail.com>
2024-06-13 17:45:29 +0100

    singly linkedlist implemented

---
+++ LinkedList.js
@@ -0,0 +1,54 @@
+function Node(element){
+    this.element = element;
+    this.next = null;
+}
+function LList(){
+    this.head = new Node("head");
+    this.find = find;
+    this.insert = insert;
+    this.display = display;
+    this.findPrevious = findPrevious;
+    this.remove = remove;
+}
+
+function find(item){
+    let currNode = this.head;
+    while(currNode.element != item){
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+function findPrevious(item){
+    let currNode = this.head;
+    while(!(currNode.next == null) && (currNode.next.element != item)){
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+function remove(item){
+    let prevNode = this.findPrevious(item);
+    if(!(prevNode.next == null)){
+        prevNode.next = prevNode.next.next
+    }
+}
+function insert(newElement, item){
+    let newNode = new Node(newElement);
+    let current = this.find(item);
+    newNode.next = current.next;
+    current.next = newNode;
+}
+function display(){
+    let currNode = this.head;
+    while(!(currNode.next == null)){
+        console.log(currNode.next.element);
+        currNode = currNode.next;
+    }
+}
+let cities = new LList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle", "Russellville");
+cities.insert("Alma", "Russellville");
+
+console.log(cities.remove("Carlisle"));
+console.log(cities.display());
