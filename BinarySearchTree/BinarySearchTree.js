45c31dea4dfdeb6c89e11a2a69322b38b161ccca
bello hadi <bellohadi82@gmail.com>
2025-01-25 16:33:12 +0100

    :art: added the remove node function

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -88,4 +88,41 @@ function find(data){
             return null;
         }
     }
+    return current
+}
+// removing a node in a bst
+// we have to declare two function remove and removeNode
+function removeNode(node, data){
+    if(node == null){
+        return null;
+    }
+    if(data == node.data){
+        //node has no children
+        if(node.left == null  && node.right == null){
+            return null;
+        }
+        //node has no left child
+        if(node.left == null){
+            return node.right;
+        }
+        //node has no right child
+        if(node.right == null){
+            return node.left;
+        } 
+        //node has two children
+        let tempNode = getSmallest(node.right);
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+    }else if(data < node.data){
+        node.left = removeNode(node.right, tempNode.data);
+        return node;
+    }else{
+        node.right = removeNode(node.right, data);
+        return node;
+    }
+    
+}
+function remove(data){
+    return root = removeNode(this.root, data)
 }
