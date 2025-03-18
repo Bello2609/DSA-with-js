f689149c75164904ad7c35717e67e33614036e37
bello hadi <bellohadi82@gmail.com>
2025-03-18 15:08:24 +0100

    :sparkle: implemented dfs and bfs

---
+++ Graph/Graph.js
@@ -0,0 +1,69 @@
+function Graph(v){
+    this.vertices = v;
+    this.edges = 0;
+    this.adj = [];
+    for(let i = 0; i < this.vertices; ++i){
+        this.adj[i] = [];
+        this.adj[i].push("");
+    }
+    this.addEdge = addEdge;
+    this.showGraph = showGraph;
+    this.dfs = dfs;
+    this.bfs = bfs;
+    this.marked = [];
+    for(let i = 0; i < this.vertices; ++i){
+        this.marked[i] = false;
+    }
+}
+function addEdge(v,w){
+    this.adj[v].push(w);
+    this.adj[w].push[v];
+    this.edges++;
+}
+function showGraph(){
+    for(let i = 0; i < this.vertices; ++i){
+        // console.log(i + " -> ");
+        for(let j = 0; j < this.vertices; ++j){
+            if(this.adj[i][j] != undefined){
+                console.log(this.adj[i][j] + "");
+            }
+        }
+    }
+} 
+function dfs(v){
+    this.marked[v] = true;
+    if(this.adj[v] != undefined){
+        console.log("Visited: " + v);
+    }
+    for(let w in this.adj[v]){
+        if(!this.marked[w]){
+            this.dfs(w);
+        }
+    }
+}
+function bfs(s){
+    let queue = [];
+    this.marked[s] = true;
+    queue.push(s);
+    while(queue.length > 0){
+        let v = queue.shift();
+        if(v == undefined){
+            console.log("Visited vertexb: ", v);
+        }
+        for(let w in this.adj[v]){
+            if(!this.marked[w]){
+                this.marked[w] = true;
+                queue.push(w);
+            }
+        }
+    }
+    
+}
+ let graph = new Graph(5);
+graph.addEdge(0,1);
+graph.addEdge(3,2);
+graph.addEdge(1,3);
+graph.addEdge(2,4);
+graph.showGraph();
+// graph.dfs(0);
+graph.bfs(0);
