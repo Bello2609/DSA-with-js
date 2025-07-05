b2105809f0ba7e5244bd1d43e66ed7fcdce7584c
bello hadi <bellohadi82@gmail.com>
2025-07-05 18:07:34 +0100

    :sparkles: implement quicksort

--- Sorting/Sorting.js
+++ Sorting/Sorting.js
@@ -160,6 +160,23 @@ function mergeSort(){
         step *= 2;
     }
 }
+function quickSort(list){
+    if(arr.length == 0){
+        return [];
+    }
+    let left = [];
+    let right = [];
+    let pivot = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+
+}
 let numElements = 10;
 
 let mynums = new CArray(numElements);
