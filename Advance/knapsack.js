1048597a82d2a328ab19bdd8640cd62589f39aac
bello hadi <bellohadi82@gmail.com>
2025-07-15 19:42:15 +0100

    :sparkles: add dynamic knapsack approach

--- Advance/knapsack.js
+++ Advance/knapsack.js
@@ -16,3 +16,24 @@ let size = [3,4,7,8,9];
 let capacity = 16;
 let n = 5;
 console.log(knapsack(capacity, size, value, n));
+
+function dKnapsack(capacity, size, value, n){
+    let k = [];
+    for(let i = 0; i <= capacity + 1; ++i){
+        k[i] = [];
+    }
+    for(let i = 0; i <= n; i++){
+        for(w = 0; w <= capacity; w++){
+            if(i == 0 || w == 0){
+                k[i][w] = 0;
+            }else if(size[i-1] <= w){
+                k[i][w] = max(value[i-1] + k[i-1][w-size[i-1]], k[i-1][w]);
+            }else{
+                k[i][w] = k[i-1][w];
+            }
+            console.log(k[i][w] + " ");
+        }
+    }
+
+    return k[n][capacity];
+}
