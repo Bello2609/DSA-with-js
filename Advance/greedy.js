bd174a3cba842bbee7f6e4e7dff7d4cd13f5d894
bello hadi <bellohadi82@gmail.com>
2025-07-16 14:45:47 +0100

    :sparkles: implemented greedy algorithm for knapsack problem

--- Advance/greedy.js
+++ Advance/greedy.js
@@ -36,3 +36,30 @@ let origAmt = .63;
 let coins = [];
 console.log(makeChange(origAmt, coins));
 console.log(showChange(coins));
+
+
+
+
+function ksack(values, weights, capacity) {
+    var load = 0;
+    var i = 0;
+    var w = 0;
+    while (load < capacity && i < 4) {
+        if (weights[i] <= (capacity-load)) {
+            w += values[i];
+            load += weights[i];
+        }
+        else {
+            var r = (capacity-load)/weights[i];
+            w += r * values[i];
+            load += weights[i];
+        }
+        ++i;
+    }
+    return w;
+}
+let items = ["A", "B", "C", "D"];
+let values = [50, 140, 60, 60];
+let weights = [5, 20, 10, 12];
+let capacity = 30;
+console.log(ksack(values, weights, capacity));
